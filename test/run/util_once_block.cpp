1380b8dc35e0ca416038d7a3f87d244575654d3a
Andrey Semashev <andrey.semashev@gmail.com>
2013-04-20 11:15:36 +0000

    Made tests build in multithreaded environment by default (this can still be changed by specifying threading=single in the command line). The util_once_block test updated to be usable in singlethreaded builds.
    [SVN r83977]

--- test/run/util_once_block.cpp
+++ test/run/util_once_block.cpp
@@ -17,10 +17,10 @@
 #define BOOST_TEST_MODULE util_once_block
 
 #include <boost/log/utility/once_block.hpp>
+#include <boost/test/included/unit_test.hpp>
 
 #if !defined(BOOST_LOG_NO_THREADS)
 
-#include <boost/test/included/unit_test.hpp>
 #include <boost/thread/thread.hpp>
 #include <boost/thread/mutex.hpp>
 #include <boost/thread/locks.hpp>
@@ -50,7 +50,7 @@ void initialize_variable()
 void once_block_flag_thread()
 {
     int my_once_value = 0;
-    for (unsigned i = 0; i < LOOP_COUNT; ++i)
+    for (unsigned int i = 0; i < LOOP_COUNT; ++i)
     {
         BOOST_LOG_ONCE_BLOCK_FLAG(flag)
         {
@@ -74,7 +74,7 @@ BOOST_AUTO_TEST_CASE(once_block_flag)
 
     try
     {
-        for (unsigned i = 0; i < THREAD_COUNT; ++i)
+        for (unsigned int i = 0; i < THREAD_COUNT; ++i)
         {
             group.create_thread(&once_block_flag_thread);
         }
@@ -95,7 +95,7 @@ int var_to_init_once = 0;
 void once_block_thread()
 {
     int my_once_value = 0;
-    for (unsigned i = 0; i < LOOP_COUNT; ++i)
+    for (unsigned int i = 0; i < LOOP_COUNT; ++i)
     {
         BOOST_LOG_ONCE_BLOCK()
         {
@@ -121,7 +121,7 @@ BOOST_AUTO_TEST_CASE(once_block)
 
     try
     {
-        for (unsigned i = 0; i < THREAD_COUNT; ++i)
+        for (unsigned int i = 0; i < THREAD_COUNT; ++i)
         {
             group.create_thread(&once_block_thread);
         }
@@ -142,8 +142,8 @@ struct my_exception
 {
 };
 
-unsigned pass_counter = 0;
-unsigned exception_counter = 0;
+unsigned int pass_counter = 0;
+unsigned int exception_counter = 0;
 
 void once_block_with_exception_thread()
 {
@@ -173,7 +173,7 @@ BOOST_AUTO_TEST_CASE(once_block_retried_on_exception)
 
     try
     {
-        for (unsigned i = 0; i < THREAD_COUNT; ++i)
+        for (unsigned int i = 0; i < THREAD_COUNT; ++i)
         {
             group.create_thread(&once_block_with_exception_thread);
         }
@@ -184,6 +184,97 @@ BOOST_AUTO_TEST_CASE(once_block_retried_on_exception)
         group.interrupt_all();
         group.join_all();
         throw;
+    }
+
+    BOOST_CHECK_EQUAL(pass_counter, 3u);
+    BOOST_CHECK_EQUAL(exception_counter, 2u);
+}
+
+#else // BOOST_LOG_NO_THREADS
+
+namespace logging = boost::log;
+
+enum config
+{
+    LOOP_COUNT = 100
+};
+
+logging::once_block_flag flag = BOOST_LOG_ONCE_BLOCK_INIT;
+int var_to_init_once_flag = 0;
+
+void initialize_variable()
+{
+    ++var_to_init_once_flag;
+}
+
+
+// The test checks if the BOOST_LOG_ONCE_BLOCK_FLAG macro works
+BOOST_AUTO_TEST_CASE(once_block_flag)
+{
+    for (unsigned int i = 0; i < LOOP_COUNT; ++i)
+    {
+        BOOST_LOG_ONCE_BLOCK_FLAG(flag)
+        {
+            initialize_variable();
+        }
+
+        if (var_to_init_once_flag != 1)
+        {
+            break;
+        }
+    }
+
+    BOOST_CHECK_EQUAL(var_to_init_once_flag, 1);
+}
+
+int var_to_init_once = 0;
+
+// The test checks if the BOOST_LOG_ONCE_BLOCK macro works
+BOOST_AUTO_TEST_CASE(once_block)
+{
+    for (unsigned int i = 0; i < LOOP_COUNT; ++i)
+    {
+        BOOST_LOG_ONCE_BLOCK()
+        {
+            ++var_to_init_once;
+        }
+
+        if (var_to_init_once != 1)
+        {
+            break;
+        }
+    }
+
+    BOOST_CHECK_EQUAL(var_to_init_once, 1);
+}
+
+struct my_exception
+{
+};
+
+unsigned int pass_counter = 0;
+unsigned int exception_counter = 0;
+
+// The test verifies that the once_block flag is not set if an exception is thrown from the once-block
+BOOST_AUTO_TEST_CASE(once_block_retried_on_exception)
+{
+    for (unsigned int i = 0; i < LOOP_COUNT; ++i)
+    {
+        try
+        {
+            BOOST_LOG_ONCE_BLOCK()
+            {
+                ++pass_counter;
+                if (pass_counter < 3)
+                {
+                    throw my_exception();
+                }
+            }
+        }
+        catch (my_exception&)
+        {
+            ++exception_counter;
+        }
     }
 
     BOOST_CHECK_EQUAL(pass_counter, 3u);
