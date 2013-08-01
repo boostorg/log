b701df1fab151056d7fe772f3d82065890659cd9
Andrey Semashev <andrey.semashev@gmail.com>
2013-08-01 21:17:11 +0000

    Disabled warning suppression pragmas for Intel compiler since it doesn't support them.
    [SVN r85185]

--- include/boost/log/detail/header.hpp
+++ include/boost/log/detail/header.hpp
@@ -37,7 +37,8 @@
 // function marked as __forceinline not inlined
 #pragma warning(disable: 4714)
 
-#elif defined(__GNUC__) && (__GNUC__ * 100 + __GNUC_MINOR__) >= 406
+#elif defined(__GNUC__) && !(defined(__INTEL_COMPILER) || defined(__ICL) || defined(__ICC) || defined(__ECC)) \
+    && (__GNUC__ * 100 + __GNUC_MINOR__) >= 406
 
 #pragma GCC diagnostic push
 // 'var' defined but not used
