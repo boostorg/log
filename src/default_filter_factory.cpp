fbc6662b9edda672d4c7bac6e0183b81d870265b
Andrey Semashev <andrey.semashev@gmail.com>
2013-08-24 17:13:18 +0000

    Fixed construction of filters with numeric operands. The constructed filters will now also work with string attribute values. Otherwise it was not possible to create filters on string attribute values that use numeric operands. Added a test for filter parser.
    [SVN r85448]

--- src/default_filter_factory.cpp
+++ src/default_filter_factory.cpp
@@ -18,6 +18,10 @@
 #include <string>
 #include <boost/move/core.hpp>
 #include <boost/move/utility.hpp>
+#include <boost/utility/enable_if.hpp>
+#include <boost/mpl/copy.hpp>
+#include <boost/mpl/contains.hpp>
+#include <boost/mpl/back_inserter.hpp>
 #include <boost/spirit/include/qi_core.hpp>
 #include <boost/spirit/include/qi_eoi.hpp>
 #include <boost/spirit/include/qi_as.hpp>
@@ -29,7 +33,6 @@
 #include <boost/log/utility/functional/begins_with.hpp>
 #include <boost/log/utility/functional/ends_with.hpp>
 #include <boost/log/utility/functional/contains.hpp>
-#include <boost/log/utility/functional/bind.hpp>
 #include <boost/log/utility/functional/as_action.hpp>
 #include <boost/log/detail/code_conversion.hpp>
 #if defined(BOOST_LOG_USE_CHAR) && defined(BOOST_LOG_USE_WCHAR_T)
@@ -52,101 +55,144 @@ namespace aux {
 
 BOOST_LOG_ANONYMOUS_NAMESPACE {
 
-template< typename RelationT >
-struct on_integral_argument
-{
-    typedef void result_type;
+#if defined(BOOST_LOG_USE_CHAR) && defined(BOOST_LOG_USE_WCHAR_T)
 
-    on_integral_argument(attribute_name const& name, filter& f) : m_name(name), m_filter(f)
+//! A special filtering predicate that adopts the string operand to the attribute value character type
+template< typename RelationT >
+class string_predicate :
+    public RelationT
+{
+private:
+    template< typename StringT >
+    struct initializer
     {
+        typedef void result_type;
+
+        explicit initializer(StringT const& val) : m_initializer(val)
+        {
+        }
+
+        template< typename T >
+        result_type operator() (T& val) const
+        {
+            try
+            {
+                log::aux::code_convert(m_initializer, val);
+            }
+            catch (...)
+            {
+                val.clear();
+            }
+        }
+
+    private:
+        StringT const& m_initializer;
+    };
+
+public:
+    typedef RelationT relation_type;
+    typedef typename relation_type::result_type result_type;
+
+    template< typename StringT >
+    string_predicate(relation_type const& rel, StringT const& operand) : relation_type(rel)
+    {
+        fusion::for_each(m_operands, initializer< StringT >(operand));
     }
 
-    result_type operator() (long val) const
+    template< typename T >
+    typename boost::enable_if<
+        mpl::contains< log::string_types::type, T >,
+        result_type
+    >::type operator() (T const& val) const
     {
-        typedef binder2nd< RelationT, long > predicate;
-        m_filter = predicate_wrapper< log::numeric_types::type, predicate >(m_name, predicate(RelationT(), val));
+        typedef std::basic_string< typename T::value_type > operand_type;
+        return relation_type::operator() (val, fusion::at_key< operand_type >(m_operands));
     }
 
 private:
-    attribute_name m_name;
-    filter& m_filter;
+    fusion::set< std::string, std::wstring > m_operands;
 };
 
-template< typename RelationT >
-struct on_fp_argument
-{
-    typedef void result_type;
+#else // defined(BOOST_LOG_USE_CHAR) && defined(BOOST_LOG_USE_WCHAR_T)
 
-    on_fp_argument(attribute_name const& name, filter& f) : m_name(name), m_filter(f)
+//! A special filtering predicate that adopts the string operand to the attribute value character type
+template< typename RelationT >
+class string_predicate :
+    public RelationT
+{
+private:
+#if defined(BOOST_LOG_USE_CHAR)
+    typedef std::basic_string< char > string_type;
+#elif defined(BOOST_LOG_USE_WCHAR_T)
+    typedef std::basic_string< wchar_t > string_type;
+#else
+#error Boost.Log: Inconsistent character configuration
+#endif
+
+public:
+    typedef RelationT relation_type;
+    typedef typename relation_type::result_type result_type;
+
+public:
+    string_predicate(relation_type const& rel, string_type const& operand) : relation_type(rel), m_operand(operand)
     {
     }
 
-    result_type operator() (double val) const
+    template< typename T >
+    typename boost::enable_if<
+        mpl::contains< log::string_types::type, T >,
+        result_type
+    >::type operator() (T const& val) const
     {
-        typedef binder2nd< RelationT, double > predicate;
-        m_filter = predicate_wrapper< log::floating_point_types::type, predicate >(m_name, predicate(RelationT(), val));
+        return relation_type::operator() (val, m_operand);
     }
 
 private:
-    attribute_name m_name;
-    filter& m_filter;
+    const string_type m_operand;
 };
+
+#endif // defined(BOOST_LOG_USE_CHAR) && defined(BOOST_LOG_USE_WCHAR_T)
+
+//! A filtering predicate for numeric relations
+template< typename NumericT, typename RelationT >
+class numeric_predicate :
+    public string_predicate< RelationT >
+{
+    typedef string_predicate< RelationT > base_type;
+
+public:
+    typedef NumericT numeric_type;
+    typedef typename base_type::relation_type relation_type;
+    typedef typename base_type::result_type result_type;
+
+public:
+    template< typename StringT >
+    numeric_predicate(relation_type const& rel, StringT const& string_operand, numeric_type numeric_operand) :
+        base_type(rel, string_operand),
+        m_numeric_operand(numeric_operand)
+    {
+    }
+
+    using base_type::operator();
+
+    template< typename T >
+    typename boost::disable_if<
+        mpl::contains< log::string_types::type, T >,
+        result_type
+    >::type operator() (T const& val) const
+    {
+        return relation_type::operator() (val, m_numeric_operand);
+    }
+
+private:
+    const numeric_type m_numeric_operand;
+};
+
 
 template< typename RelationT >
 struct on_string_argument
 {
     typedef void result_type;
-
-#if defined(BOOST_LOG_USE_CHAR) && defined(BOOST_LOG_USE_WCHAR_T)
-    //! A special filtering predicate that adopts the string operand to the attribute value character type
-    struct predicate :
-        public RelationT
-    {
-        template< typename StringT >
-        struct initializer
-        {
-            typedef void result_type;
-
-            explicit initializer(StringT const& val) : m_initializer(val)
-            {
-            }
-
-            template< typename T >
-            result_type operator() (T& val) const
-            {
-                try
-                {
-                    log::aux::code_convert(m_initializer, val);
-                }
-                catch (...)
-                {
-                    val.clear();
-                }
-            }
-
-        private:
-            StringT const& m_initializer;
-        };
-
-        typedef typename RelationT::result_type result_type;
-
-        template< typename StringT >
-        predicate(RelationT const& rel, StringT const& operand) : RelationT(rel)
-        {
-            fusion::for_each(m_operands, initializer< StringT >(operand));
-        }
-
-        template< typename T >
-        result_type operator() (T const& val) const
-        {
-            typedef std::basic_string< typename T::value_type > operand_type;
-            return RelationT::operator() (val, fusion::at_key< operand_type >(m_operands));
-        }
-
-    private:
-        fusion::set< std::string, std::wstring > m_operands;
-    };
-#endif
 
     on_string_argument(attribute_name const& name, filter& f) : m_name(name), m_filter(f)
     {
@@ -155,14 +201,62 @@ struct on_string_argument
     template< typename StringT >
     result_type operator() (StringT const& val) const
     {
-#if !defined(BOOST_LOG_USE_CHAR) || !defined(BOOST_LOG_USE_WCHAR_T)
-        typedef binder2nd< RelationT, StringT > predicate;
-#endif
-        m_filter = predicate_wrapper< log::string_types::type, predicate >(m_name, predicate(RelationT(), val));
+        typedef string_predicate< RelationT > predicate;
+        m_filter = predicate_wrapper< log::string_types, predicate >(m_name, predicate(RelationT(), val));
     }
 
 private:
     attribute_name m_name;
+    filter& m_filter;
+};
+
+template< typename RelationT, typename StringT >
+struct on_integral_argument
+{
+    typedef void result_type;
+
+    on_integral_argument(attribute_name const& name, StringT const& operand, filter& f) : m_name(name), m_operand(operand), m_filter(f)
+    {
+    }
+
+    result_type operator() (long val) const
+    {
+        typedef numeric_predicate< long, RelationT > predicate;
+        typedef mpl::copy<
+            log::string_types,
+            mpl::back_inserter< log::numeric_types >
+        >::type value_types;
+        m_filter = predicate_wrapper< value_types, predicate >(m_name, predicate(RelationT(), m_operand, val));
+    }
+
+private:
+    attribute_name m_name;
+    StringT const& m_operand;
+    filter& m_filter;
+};
+
+template< typename RelationT, typename StringT >
+struct on_fp_argument
+{
+    typedef void result_type;
+
+    on_fp_argument(attribute_name const& name, StringT const& operand, filter& f) : m_name(name), m_operand(operand), m_filter(f)
+    {
+    }
+
+    result_type operator() (double val) const
+    {
+        typedef numeric_predicate< double, RelationT > predicate;
+        typedef mpl::copy<
+            log::string_types,
+            mpl::back_inserter< log::floating_point_types >
+        >::type value_types;
+        m_filter = predicate_wrapper< value_types, predicate >(m_name, predicate(RelationT(), m_operand, val));
+    }
+
+private:
+    attribute_name m_name;
+    StringT const& m_operand;
     filter& m_filter;
 };
 
@@ -243,8 +337,8 @@ filter default_filter_factory< CharT >::parse_argument(attribute_name const& nam
     const qi::real_parser< double, qi::strict_real_policies< double > > real_;
 
     filter f;
-    const on_fp_argument< RelationT > on_fp(name, f);
-    const on_integral_argument< RelationT > on_int(name, f);
+    const on_fp_argument< RelationT, string_type > on_fp(name, arg, f);
+    const on_integral_argument< RelationT, string_type > on_int(name, arg, f);
     const on_string_argument< RelationT > on_str(name, f);
 
     const bool res = qi::parse
