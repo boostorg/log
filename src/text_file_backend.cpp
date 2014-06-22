dfb3049a7cd9b6949372ea774bd71ddd047e235a
Andrey Semashev <andrey.semashev@gmail.com>
2014-06-22 18:46:17 +0400

    Extracted text_multifile_backend implementation to a separate file.

--- src/text_file_backend.cpp
+++ src/text_file_backend.cpp
@@ -54,7 +54,6 @@
 #include <boost/log/exceptions.hpp>
 #include <boost/log/attributes/time_traits.hpp>
 #include <boost/log/sinks/text_file_backend.hpp>
-#include <boost/log/sinks/text_multifile_backend.hpp>
 
 #if !defined(BOOST_LOG_NO_THREADS)
 #include <boost/thread/locks.hpp>
@@ -1353,67 +1352,6 @@ BOOST_LOG_API uintmax_t text_file_backend::scan_for_files(file::scan_method meth
     else
     {
         BOOST_LOG_THROW_DESCR(setup_error, "File collector is not set");
-    }
-}
-
-
-////////////////////////////////////////////////////////////////////////////////
-//  Multifile sink backend implementation
-////////////////////////////////////////////////////////////////////////////////
-//! Sink implementation data
-struct text_multifile_backend::implementation
-{
-    //! File name composer
-    file_name_composer_type m_FileNameComposer;
-    //! Base path for absolute path composition
-    const filesystem::path m_BasePath;
-    //! File stream
-    filesystem::ofstream m_File;
-
-    implementation() :
-        m_BasePath(filesystem::current_path())
-    {
-    }
-
-    //! Makes relative path absolute with respect to the base path
-    filesystem::path make_absolute(filesystem::path const& p)
-    {
-        return filesystem::absolute(p, m_BasePath);
-    }
-};
-
-//! Default constructor
-BOOST_LOG_API text_multifile_backend::text_multifile_backend() : m_pImpl(new implementation())
-{
-}
-
-//! Destructor
-BOOST_LOG_API text_multifile_backend::~text_multifile_backend()
-{
-    delete m_pImpl;
-}
-
-//! The method sets the file name composer
-BOOST_LOG_API void text_multifile_backend::set_file_name_composer_internal(file_name_composer_type const& composer)
-{
-    m_pImpl->m_FileNameComposer = composer;
-}
-
-//! The method writes the message to the sink
-BOOST_LOG_API void text_multifile_backend::consume(record_view const& rec, string_type const& formatted_message)
-{
-    typedef file_char_traits< string_type::value_type > traits_t;
-    if (!m_pImpl->m_FileNameComposer.empty())
-    {
-        filesystem::path file_name = m_pImpl->make_absolute(m_pImpl->m_FileNameComposer(rec));
-        filesystem::create_directories(file_name.parent_path());
-        m_pImpl->m_File.open(file_name, std::ios_base::out | std::ios_base::app);
-        if (m_pImpl->m_File.is_open())
-        {
-            m_pImpl->m_File.write(formatted_message.data(), static_cast< std::streamsize >(formatted_message.size()));
-            m_pImpl->m_File.put(traits_t::newline);
-            m_pImpl->m_File.close();
-        }
     }
 }
 
