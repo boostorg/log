89474f4a8097d419493ebe8915db363e668fef5c
Andrey Semashev <andrey.semashev@gmail.com>
2015-03-25 00:32:36 +0300

    Define ASIO config macros before using it. Use hidden visibility by default.
    Boost.ASIO should not use compiler-based TLS unless it is allowed by the user of Boost.Log, otherwise the Boost.Log config option has no practical effect.
    As a result, it becomes more probable that Boost.ASIO will be compiled with different config macros in Boost.Log and user's code. As a partial solution use
    hidden visibility when building Boost.Log shared libs and ensure it uses its local copy of Boost.ASIO. Added a config test to detect support for visibility
    command line option.
    
    Also adjusted project names of other config tests to avoid possible name clashes with other libraries and Boost.Build infrastructure.
    
    MSVC warning 4003 is now silenced on all compiler versions as apparently not only MSVC 8 is affected.

---
+++ config/visibility/visibility.cpp
@@ -0,0 +1,11 @@
+/*
+ *          Copyright Andrey Semashev 2007 - 2015.
+ * Distributed under the Boost Software License, Version 1.0.
+ *    (See accompanying file LICENSE_1_0.txt or copy at
+ *          http://www.boost.org/LICENSE_1_0.txt)
+ */
+
+int main(int, char*[])
+{
+    return 0;
+}
