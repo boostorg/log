34e68c7b5a7d885db51f877ba35c13927553d2ac
Andrey Semashev <andrey.semashev@gmail.com>
2015-03-29 20:12:58 +0300

    Ported from std::random_shuffle and std::rand to Boost.Random and custom random_shuffle implementation.

--- test/performance/record_emission.cpp
+++ test/performance/record_emission.cpp
@@ -41,7 +41,7 @@
 enum config
 {
     RECORD_COUNT = 50000000,
-    THREAD_COUNT = 3,
+    THREAD_COUNT = 8,
     SINK_COUNT = 3
 };
 
@@ -104,7 +104,7 @@ int main(int argc, char* argv[])
 //    logging::core::get()->set_filter(severity > normal); // all records pass the filter
 //    logging::core::get()->set_filter(severity > error); // all records don't pass the filter
 
-    logging::core::get()->set_filter(severity > error); // all records don't pass the filter
+//    logging::core::get()->set_filter(severity > error); // all records don't pass the filter
 
     const unsigned int record_count = RECORD_COUNT / THREAD_COUNT;
     boost::barrier bar(THREAD_COUNT);
